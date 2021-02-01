f309b68877783f5e032e67a18563b70f42ef4711
cgbts88 <857635@qq.com>
2021-02-01 21:48:03 +0800

    2021-02-01 HOME

--- apps/worktable/views_order.py
+++ apps/worktable/views_order.py
@@ -55,6 +55,13 @@ class WorkOrderListView(APIView):
         serializer = WorkOrderLogSerializer(work_order_log[0: 10], many=True)
         return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = WorkOrderLogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class WorkOrderCreateView(MisCreateView):
     model = WorkOrder
